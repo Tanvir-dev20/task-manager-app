29d61cca2c07f756530bc8020b93c00e9075f40a
Tanvir <blackmack6677@gmail.com>
2025-10-31 23:30:00 +0600

    add logo

--- lib/ui/sreens/splash_screen.dart
+++ lib/ui/sreens/splash_screen.dart
@@ -36,7 +36,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: Center(child: SvgPicture.asset(AssetsPath.logoSvg)),
+        child: Image.asset('assets/images/education.png', height: 60),,
       ),
     );
   }
